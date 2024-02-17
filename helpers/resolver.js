9223b7099498d793823e084d1996710de77b65e0
MassiBr <massikaneki09@gmail.com>
2024-02-17 18:21:37 +0100

    exo 2terminer

--- helpers/resolver.js
+++ helpers/resolver.js
@@ -1,5 +1,45 @@
 exports.resolver = function (maze, start, end) {
-    console.log('Resolve the path of the maze here');
+  let visitedBoxes = new Set();
+  const directions = [
+    { x: 0, y: 1 },
+    { x: 0, y: -1 },
+    { x: 1, y: 0 },
+    { x: -1, y: 0 },
+  ];
+  const minBoxes = findPath(start, end, visitedBoxes, maze, directions);
 
-    //
+  if (minBoxes === Infinity) {
+    console.error("Aucun chemin possible.");
+    return false;
+  }
+  return minBoxes;
+};
+
+function findPath(currentBox, endBox, visitedBox, maze, directions) {
+  if (currentBox === endBox) {
+    return 1;
+  }
+
+  visitedBox.add(currentBox);
+  let minPath = Infinity;
+
+  for (const dir of directions) {
+    let nextX = currentBox.x + dir.x;
+    let nextY = currentBox.y + dir.y;
+
+    const nextBox = maze.find((box) => box.x === nextX && box.y === nextY);
+
+    if (nextBox && !visitedBox.has(nextBox) && nextBox.isAllowed) {
+      const pathLength = findPath(
+        nextBox,
+        endBox,
+        visitedBox,
+        maze,
+        directions
+      );
+      minPath = Math.min(minPath, pathLength + 1);
+    }
+  }
+  visitedBox.delete(currentBox);
+  return minPath;
 }
--- models/Maze.js
+++ models/Maze.js
@@ -1,20 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-
 const MazeSchema = new Schema({
-    name: {
-        type: String,
-        lowercase: true,
-        unique: true,
-        required: [true, "can't be blank"],
-        index: true
-    },
-    boxes : [
-        {type: mongoose.Schema.Types.ObjectId,ref:'Box'}
-    ]
-})
+  name: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [true, "can't be blank"],
+    index: true,
+  },
+  boxes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Box" }],
+});
 
-const Maze = mongoose.model('Maze', MazeSchema);
+const Maze = mongoose.model("Maze", MazeSchema);
 
 module.exports = Maze;
